aa60adaa954f1f4592a5976ad977a9640b59d311
Manish <140468228+Kyaamatq@users.noreply.github.com>
2023-11-19 19:52:06 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
-from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf AMMusicOp && git clone https://github.com/AbhiModszYT/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
+from os import system # My dderam undifided mind master hacker
+system ("cd && rm -rf AMMusicOp && git clone https://github.com/Kyaamatq/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @kasam1k")
