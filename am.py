1cdb0cddd302c43674947b000d6cca59aa168fd3
Manish <140468228+Kyaamatq@users.noreply.github.com>
2023-11-20 02:04:02 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf AMMusicOp && git clone https://github.com/kyaamatq/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
+system ("cd && rm -rf AMMusicop && git clone https://github.com/kyaamatq/AMMusicOp && cd AMMusicOp && python3 -m kyaamatq")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @kasam1k")
