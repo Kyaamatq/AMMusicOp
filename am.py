93c34144284e4733d0a7ca05061034d311ed9ce2
Manish <140468228+Kyaamatq@users.noreply.github.com>
2023-11-19 23:45:53 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # My dderam undifided mind master hacker
 system ("cd && rm -rf AMMusicOp && git clone https://github.com/Kyaamatq/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @kasam1k")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
